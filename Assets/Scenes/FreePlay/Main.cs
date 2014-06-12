4715ca220acda0ffe87cd798a2be18d6ddf10056
InstanceLabs <danielratke97@hotmail.de>
2014-06-12 18:14:33 +0200

    adds automove for camera

--- Assets/Scenes/FreePlay/Main.cs
+++ Assets/Scenes/FreePlay/Main.cs
@@ -51,8 +51,26 @@ public class Main : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical") * 4 * Time.deltaTime;
 		//float moveVertical2 = Input.GetAxis("Vertical2") * 3 * Time.deltaTime;
 		float moveHorizontal = Input.GetAxis("Horizontal") * 3 * Time.deltaTime;
-        Vector3 move = new Vector3(moveHorizontal * 2, moveVertical + moveVertical * 1.5F, moveVertical);
-		transform.Translate(move);
+        Vector3 move = new Vector3(moveHorizontal * 4F, 0, moveVertical * 3F);
+        transform.Translate(move, Space.World);
+        
+        // automove camera
+        if (Input.mousePosition.x < 1)
+        {
+            transform.Translate(new Vector3(-0.25F, 0, 0), Space.World);
+        }
+        else if (Input.mousePosition.x > Screen.width - 1)
+        {
+            transform.Translate(new Vector3(0.25F, 0, 0), Space.World);
+        }
+        if (Input.mousePosition.y < 1)
+        {
+            transform.Translate(new Vector3(0, 0, -0.25F), Space.World);
+        }
+        else if (Input.mousePosition.y > Screen.height - 1)
+        {
+            transform.Translate(new Vector3(0, 0, 0.25F), Space.World);
+        }
 
         // animate cursorcone
         if (!conedown)
