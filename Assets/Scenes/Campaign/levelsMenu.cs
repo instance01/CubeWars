72143bb4549843d0de86dba420d599f67781d05f
InstanceLabs <danielratke97@hotmail.de>
2014-06-12 15:52:34 +0200

    Adds Levels menu for Campaign

---
+++ Assets/Scenes/Campaign/levelsMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelsMenu : MonoBehaviour {
+
+    public GUISkin skin;
+
+	void OnGUI () {
+
+        GUI.skin = skin;
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 400, 100, 100, 100), "1\nInit"))
+        {
+			
+		}
+
+        GUI.enabled = false;
+        if (GUI.Button(new Rect(Screen.width / 2 - 300, 100, 100, 100), "2\nFirst grief"))
+        {
+			
+		}
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 200, 100, 100, 100), "3\nOver-\nwhelming"))
+        {
+			
+		}
+        GUI.enabled = true;
+	}
+}
